2e3f23427e2b13c2064e3cd69229287517a8c3b2
= <=>
2021-04-17 10:36:18 +0100

    Moved to most recent version of cite

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ rm -r code
 cp -r md src
 
 find src -name "*.md" | cut -d. -f1 | while read i; do
-	echo $i
 	markdown -o $i.html $i.md
 	rm $i.md
 done
@@ -20,10 +19,6 @@ done
 # compile and run cite
 make clean cite
 ./cite
-
-find . -name "*.html" | while read i; do
-	tidy -iq -w 80 -o $i{,} 
-done
 
 # cleanup
 rm -r src
--- cite.c
+++ cite.c
@@ -7,18 +7,38 @@
 #include "sops.h"
 
 /*
+ * Define globals
+ */
+FILE *fidx;
+int header_depth = 1;
+char pd_name[URLLEN];
+
+/* 
+ * Forward declarations
+ */
+FILE *inject_head(FILE * page);
+FILE *inject_foot(FILE * page);
+FILE *inject_contents(FILE * body, FILE * in);
+int make_page(char *fname, char *path);
+int make_dir(char *dname, char *path);
+void sf_mkdir(char *dirname);
+void set_pd_name(char *d_name, char *path);
+void build_pages(char *path);
+
+/*
  * Inject head and header text into file
  */
 FILE *inject_head(FILE * page)
 {
+	fprintf(page, "<!DOCTYPE html>\n");
 	fprintf(page, "<html>\n");
 	fprintf(page, "<head>\n");
 	fprintf(page, "<title>%s</title>\n", TITLE);
 	fprintf(page, "<link rel='stylesheet' href='%s%s'>\n", URL, CSS);
-	fprintf(page, "<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+	fprintf(page,
+		"<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
 	fprintf(page, "</head>\n");
 	fprintf(page, "<!-- Generated static page, don't edit this -->\n");
-
 	fprintf(page, "<body>\n");
 	fprintf(page, "<header><a href='%sindex.html'><h1>%s</h1></a></header>\n", URL, TITLE);
 
@@ -30,11 +50,7 @@ FILE *inject_head(FILE * page)
  */
 FILE *inject_foot(FILE * page)
 {
-	/*
-	fprintf(page, "<footer>License: CC0 | <3</footer>\n");
-	*/
 	fprintf(page, "</body>\n</html>\n");
-
 	return page;
 }
 
@@ -55,22 +71,21 @@ FILE *inject_contents(FILE * body, FILE * in)
 /*
  * Add a link to index file
  */
-void add_to_index(FILE * index, char *name, char *link)
+void add_to_index(char *name, char *link)
 {
 	char n[URLLEN];
 	scp(n, name, URLLEN);
 	sr(n, '_', ' ');
 	slcut(n, '.');
-	fprintf(index, "<div>\n<a href='%s%s'>%s</a>\n</div>\n", URL, link, n);
+	fprintf(fidx, "<div>\n<a href='%s%s'>%s</a>\n</div>\n", URL, link, n);
 }
 
 /*
  * Build page from source
  */
-int make_page(char *in, char *out)
+int inject_page(char *in, char *out)
 {
-	FILE *fin;
-	FILE *fout;
+	FILE *fin, *fout;
 
 	if ((fin = fopen(in, "r")) == NULL) {
 		printf("Couldn't open %s\n", in);
@@ -86,13 +101,11 @@ int make_page(char *in, char *out)
 
 	fclose(fin);
 	fclose(fout);
-
 	return 0;
 }
 
-/* 
-  Check for existence of a subdir in DESTDIR, and if it
- 	doesn't exist then make it
+/*
+ * Build a directory if it doesn't exist
  */
 void sf_mkdir(char *dirname)
 {
@@ -105,60 +118,100 @@ void sf_mkdir(char *dirname)
 	if (stat(dd, &s) == -1) {
 		mkdir(dd, 0700);
 	}
-} 
+}
 
-void build_pages(char *path, FILE * fidx, int depth)
+/*
+ * Make a page from source directory to destination
+ */
+int make_page(char *fname, char *path)
+{
+	char srcurl[URLLEN];
+	char desturl[URLLEN];
+	char ppd_name[URLLEN];
+	int err = 0;
+
+	scp(ppd_name, pd_name, URLLEN);
+	set_pd_name(fname, path);
+
+	scp(srcurl, SRCDIR, URLLEN);
+	sct(srcurl, pd_name, URLLEN);
+
+	scp(desturl, DESTDIR, URLLEN);
+	sct(desturl, pd_name, URLLEN);
+
+	err = inject_page(srcurl, desturl);
+
+	if (err == 0) {
+		add_to_index(fname, pd_name);
+	}
+
+	scp(pd_name, ppd_name, URLLEN);
+	return err;
+}
+
+/*
+ * Make all pages in directory and add a header to index
+ */
+int make_dir(char *d_name, char *path)
+{
+	char d_header[URLLEN];
+	char ppd_name[URLLEN];
+
+	header_depth++;
+
+	scp(ppd_name, pd_name, URLLEN);
+	set_pd_name(d_name, path);
+	sf_mkdir(pd_name);
+
+	scp(d_header, d_name, URLLEN);
+	sr(d_header, '_', ' ');
+	slcut(d_header, '.');
+
+	fprintf(fidx, "<h%d>%s</h%d>\n", header_depth, d_header, header_depth);
+	build_pages(pd_name);
+
+	header_depth--;
+	scp(pd_name, ppd_name, URLLEN);
+	return 0;
+}
+
+/*
+ * Set path + file name to pass to build functions
+ */
+void set_pd_name(char *d_name, char *path)
+{
+	scp(pd_name, path, URLLEN);
+	if (scmp(path, SRCDIR)) {
+		sct(pd_name, "/", URLLEN);
+	}
+	sct(pd_name, d_name, URLLEN);
+}
+
+/*
+ * Build all pages and subdirectories at path
+ */
+void build_pages(char *path)
 {
 	struct dirent **dirlist;
 	struct dirent *dir;
 	char fullpath[URLLEN];
-	char srcurl[URLLEN];
-	char desturl[URLLEN];
-	char pd_name[URLLEN];
-	char dt[URLLEN];
-	int n, berr;
-	int m = 0;
+	int m, n;
 
 	scp(fullpath, SRCDIR, URLLEN);
 	sct(fullpath, path, URLLEN);
 
 	n = scandir(fullpath, &dirlist, NULL, alphasort);
-
-	
 	if (n == -1) {
-		perror("scandir");
+		printf("ERROR: %s\n", fullpath);
 		exit(EXIT_FAILURE);
 	}
-	
-	n = scandir(fullpath, &dirlist, NULL, alphasort);
-	m = 0;
 
-	while (m < n) {
-		dir = dirlist[m];
-
-		scp(pd_name, path, URLLEN);
-		if (scmp(path, SRCDIR)) {
-			sct(pd_name, "/", URLLEN);
-		}
-		sct(pd_name, dir->d_name, URLLEN);
+	while (n--) {
+		dir = dirlist[n];
 
 		if (dir->d_type == DT_REG) {
-			/* cat together link to file */
-			scp(srcurl, SRCDIR, URLLEN);
-			sct(srcurl, pd_name, URLLEN);
-
-			/* cat together destination */
-			scp(desturl, DESTDIR, URLLEN);
-			sct(desturl, pd_name, URLLEN);
-
-			berr = make_page(srcurl, desturl);
-
-			/* cat together link for index.html */
-			if (berr == 0) {
-				add_to_index(fidx, dir->d_name, pd_name);
-			}
+			make_page(dir->d_name, path);
 		}
-		m++;
 	}
 
 	n = scandir(fullpath, &dirlist, NULL, alphasort);
@@ -166,36 +219,14 @@ void build_pages(char *path, FILE * fidx, int depth)
 
 	while (m < n) {
 		dir = dirlist[m];
-
-		scp(pd_name, path, URLLEN);
-		if (scmp(path, SRCDIR)) {
-			sct(pd_name, "/", URLLEN);
-		}
-		sct(pd_name, dir->d_name, URLLEN);
 
 		if (dir->d_type == DT_DIR) {
 			if (scmp(dir->d_name, ".") && scmp(dir->d_name, "..")) {
-				sf_mkdir(pd_name);		
-
-				scp(dt, dir->d_name, URLLEN);
-				sr(dt, '_', ' ');
-				slcut(dt, '.');	
-
-				depth++;
-
-				fprintf(fidx, "<h%d>%s</h%d>", depth, dt, depth);
-			
-				/* build pages in subdirectory */
-				build_pages(pd_name, fidx, depth);
-
-				depth--;
+				make_dir(dir->d_name, path);
 			}
 		}
-
 		m++;
 	}
-
-
 
 	free(dirlist);
 	free(dir);
@@ -204,24 +235,18 @@ void build_pages(char *path, FILE * fidx, int depth)
 int main(void)
 {
 	char idx[URLLEN];
-	FILE *fidx;
-
 	scp(idx, DESTDIR, URLLEN);
 	sct(idx, "index.html", URLLEN);
 
 	fidx = fopen(idx, "w");
-
 	if (fidx == NULL) {
 		printf("Couldn't create index.html\n");
 		return 1;
 	}
 
 	fidx = inject_head(fidx);
-
-	build_pages("", fidx, 1);
-
+	build_pages("");
 	fclose(inject_foot(fidx));
 
 	return 0;
-
 }
