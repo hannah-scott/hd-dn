96f8799e7e24bacf1e5a7756dffdd33c14f33779
hannah <hannahscott@tutanota.com>
2022-08-16 21:39:16 +0100

    Fixes links on meta

--- make_meta.sh
+++ make_meta.sh
@@ -6,7 +6,7 @@ rm meta/index.html
 # get list of every page that's not an index
 echo "" > temp
 find . -name "*.html" -not \( -name "draft*" -o -name "*index*" \) | sed -e "s/\/\([a-z-]*\.html\)/~\1/g" | sort -t~ -k2 | sed -e "s/~/\//g" | while read l; do
-    echo "<li><a href=\"$l\">" >> temp
+    echo "<li><a href=\"$(echo $l | cut -d. -f2-)\">" >> temp
     echo "$(echo $l | rev | cut -d/ -f1 | rev)" >> temp
     echo "</a></li>" >> temp
 done
