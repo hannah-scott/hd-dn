0301be1b4e489f3258f8160968ca4c67c3ebba1e
hannah <hannahscott@tutanota.com>
2022-09-06 17:55:35 +0100

    Changes max image size from 50KB to 100KB

--- img/resize.sh
+++ img/resize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp backups/*.jpg .
 for file in *.jpg; do
-    convert $file -strip -resize 720 -define jpeg:extent=50KB $file
+    convert $file -strip -resize 720 -define jpeg:extent=100KB $file
 done
