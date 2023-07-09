70200aa462e64cc02d60147d6565620ad072d423
hannah <hannahscott@tutanota.com>
2023-07-09 21:05:54 +0100

    Adds cron vars

--- update-hd-dn.sh
+++ update-hd-dn.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Script to autoupdate website based on git repo status
+
+# Set some environment variables. We need these for cron
+XDG_RUNTIME_DIR=/run/user/$(id -u)
+DBUS_SESSION_BUS_ADDRESS=unix:path=${XDG_RUNTIME_DIR}/bus
+export DBUS_SESSION_BUS_ADDRESS XDG_RUNTIME_DIR
+
 cd /home/hd-dn/hd-dn
 git fetch origin
 if git status | grep -q behind; then
